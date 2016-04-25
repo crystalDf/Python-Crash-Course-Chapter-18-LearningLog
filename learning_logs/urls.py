7027f9cec26a97cd9c3dd680972145fc2698475f
crystalDf <chendong333@gmail.com>
2016-04-25 22:22:24 +0800

    Adding New Topics

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
 
 	# Detail page for a single topic
 	url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+
+	# Page for adding a new topic
+	url(r'^new_topic/$', views.new_topic, name='new_topic'),
 ]
