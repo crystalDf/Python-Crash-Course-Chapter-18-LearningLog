1b05ae1cc5d4e2c10365a7ed4d3ee003acf3a1f4
crystalDf <chendong333@gmail.com>
2016-04-29 22:30:12 +0800

    The Registration Page

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,12 @@ from django.contrib.auth.views import login
 from . import views
 
 urlpatterns = [
-	#Login page
+	# Login page
 	url(r'^login/$', login, {'template_name': 'users/login.html'}, name='login'),
+
+	# Logout page
+	url(r'^logout/$', views.logout_view, name='logout'),
+
+	# Registration page
+	url(r'^register/$', views.register, name='register')
 ]
