32cb4a27823369c72def06572ac364a0d44aa81e
crystalDf <chendong333@gmail.com>
2016-04-30 22:00:22 +0800

    Allowing Users to Own Their Data

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Topic
 from .models import Entry
@@ -13,19 +15,26 @@ def index(request):
 	"""The home page for Learning Log"""
 	return render(request, 'learning_logs/index.html')
 
+@login_required
 def topics(request):
 	"""Show all topics."""
-	topics = Topic.objects.order_by('date_added')
+	topics = Topic.objects.filter(owner=request.user).order_by('date_added')
 	context = {'topics': topics}
 	return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
 	"""Show a single topic and all its entries."""
 	topic = Topic.objects.get(id=topic_id)
+
+	# Make sure the topic belongs to the current user.
+	check_topic_owner(request, topic)
+
 	entries = topic.entry_set.order_by('-date_added')
 	context = {'topic': topic, 'entries': entries}
 	return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
 	"""Add a new topic."""
 	if request.method != 'POST':
@@ -35,15 +44,20 @@ def new_topic(request):
 		# POST data submitted; process data.
 		form = TopicForm(request.POST)
 		if form.is_valid():
-			form.save()
+			new_topic = form.save(commit=False)
+			new_topic.owner=request.user
+			new_topic.save()
 			return HttpResponseRedirect(reverse('learning_logs:topics'))
 
 	context = {'form': form}
 	return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
 	"""Add a new entry for a particular topic."""
 	topic = Topic.objects.get(id=topic_id)
+
+	check_topic_owner(request, topic)
 	
 	if request.method != 'POST':
 		# No data submitted; create a blank form.
@@ -60,10 +74,13 @@ def new_entry(request, topic_id):
 	context = {'topic': topic, 'form': form}
 	return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
 	"""Edit an existing entry."""
 	entry = Entry.objects.get(id=entry_id)
 	topic = entry.topic
+
+	check_topic_owner(request, topic)
 
 	if request.method != 'POST':
 		# Initial request; pre-fill form with the current entry.
@@ -78,4 +95,6 @@ def edit_entry(request, entry_id):
 	context = {'entry': entry, 'topic': topic, 'form': form}
 	return render(request, 'learning_logs/edit_entry.html', context)
 
-
+def check_topic_owner(request, topic):
+	if topic.owner != request.user:
+		raise Http404
